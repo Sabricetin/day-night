af121a9670497998cc608f596af5a9455d8792b5
Sabricetin <sabriyazilim@gmail.com>
2024-02-11 22:44:07 +0300

    first commit

---
+++ Day-Night/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Day-Night
+//
+//  Created by Sabri Çetin on 11.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        ang
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day-Night/Day_NightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_NightApp.swift
+//  Day-Night
+//
+//  Created by Sabri Çetin on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Day_NightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
